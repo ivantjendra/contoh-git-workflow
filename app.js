9a2b5c5dbbc285094e759dca686cd2cf500770cf
ivantjendra <ivantjendra@gmail.com>
2024-06-19 09:53:52 +0700

    nambahin app.get /register

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/register', (req, res) => {
+  try {
+    res.send('register berhasil')
+  } catch(err) {
+    console.log(err)
+  }
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
